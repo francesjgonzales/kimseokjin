b180157f3c65de2265fdf6ec320b8af448b05d17
francesjgonzales <francesj7837@gmail.com>
2024-04-13 23:27:02 -0600

    added images and js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("hi");
 
 //Get Month
-const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+/* const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const currentDate = new Date();
 let updateMonth = month[currentDate.getMonth()];
-document.querySelector(".month").innerHTML = updateMonth;
+document.querySelector(".month").innerHTML = `<p class="text-[#9338ff] text-2xl">Today is ${updateMonth}</p>`; */
 
 //Get target date
 /* const x = new Date
@@ -13,7 +13,7 @@ const day = document.querySelector(".day").innerHTML = x.getDay() */
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 2024 00:00:00").getTime();
+const countDownDate = new Date("June 2024 04:47:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -31,12 +31,12 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.querySelector(".timer").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.querySelector(".timer").innerHTML = `<p class="timer text-[#9338ff] text-5xl"> ${days}d  ${hours} h
+        ${minutes} m  ${seconds} s </p>`;
 
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.querySelector(".timer").innerHTML = "EXPIRED";
+        document.querySelector(".timer").innerHTML = `<p class="timer text-[#9338ff] text-9xl">Welcome back, Jin!</p>`;
     }
 }, 1000);
